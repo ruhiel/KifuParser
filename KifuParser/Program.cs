d48574a19e5f6baa3fcda8ff2d8673bb4e3d47d5
ruhiel_murrue <ruhiel1983@gmail.com>
2017-07-01 00:20:41 +0900

    初回コミット

--- KifuParser/Program.cs
+++ KifuParser/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sprache;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace KifuParser
     {
         static void Main(string[] args)
         {
+            var move =
+                from line in Parse.Number.Token()
+                from srcX in Parse.Regex("[１２３４５６７８９]")
+                from srcY in Parse.Regex("[１２３４５６７８９]")
+                    //from piece in Parse.Regex("玉飛角金銀桂香歩龍馬と")
+                select new Move() { Line = line, SrcX = srcX, SrcY = srcY };
+
+            var nullparse =
+                from v in Parse.Regex(".+").Token()
+                select new Move();
+
+            Parser<IEnumerable<Move>> moves = move.Or(nullparse).Many().End();
+
+            // StreamReader の新しいインスタンスを生成する
+            var cReader = (
+                new System.IO.StreamReader(@"TextFile1.txt", Encoding.Default)
+            );
+
+            // ファイルの最後まで読み込む
+            var stBuffer = cReader.ReadToEnd();
+
+            // cReader を閉じる (正しくは オブジェクトの破棄を保証する を参照)
+            cReader.Close();
+
+            var list = moves.Parse(stBuffer);
+            foreach(var m in list)
+            {
+                Console.WriteLine(m.Line);
+            }
         }
     }
 }
