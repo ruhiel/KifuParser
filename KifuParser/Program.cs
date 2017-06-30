13939d0bf4c3c5daaf25569bb98b1892e1dc61b5
ruhiel_murrue <ruhiel1983@gmail.com>
2017-07-01 00:14:55 +0900

    プロジェクト ファイルを追加します。

---
+++ KifuParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KifuParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
